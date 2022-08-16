7d6cf3fde311de90f1a43e9f46b59d268879912d
kimkonpig <linggooling@gmail.com>
2022-08-16 09:45:06 +0900

    BOJ 문제풀이 - 1316

--- BOJ/python_1316.py
+++ BOJ/python_1316.py
@@ -6,11 +6,30 @@ for case in range(T):
     flag = False
 
     for i in range(0, len(str)-1):
-        for j in range(i+1, len(str)-1):
-            if str[i] == str[j+1]:
+            if str[i] == str[i+1]:
+                pass
+            elif str[i] in str[i+1:]:
                 flag = True
+                break
 
     if flag == False:
         cnt += 1
     
 print(cnt)
+
+
+
+# T = int(input())
+# result = T
+
+# for case in range(T):
+#     str = input()
+
+#     for i in range(0, len(str)-1):
+#         if str[i] == str[i+1]:
+#             pass
+#         elif str[i] in str[i+1:]:
+#             result -= 1
+#             break
+    
+# print(result)
